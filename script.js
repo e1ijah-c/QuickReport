4993d5a2e1037c239d5f9554e0f8d007247b9bd7
e1ijah-c <elcxd99@gmail.com>
2025-07-02 17:22:29 +0800

    fixing bug regarding reqiure term

--- script.js
+++ script.js
@@ -254,11 +254,4 @@ if (typeof module != 'undefined' && typeof module.exports != 'undefined') {
 
 
 
-var getCaretCoordinates = require('textarea-caret');
-
-document.querySelector('textarea').addEventListener('input', function () {
-  var caret = getCaretCoordinates(this, this.selectionEnd);
-  console.log('(top, left, height) = (%s, %s, %s)', caret.top, caret.left, caret.height);
-})
-
 
