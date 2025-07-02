834d1748a1aaef80e2010968c18d6d20f7711622
e1ijah-c <elcxd99@gmail.com>
2025-07-02 12:46:54 +0800

    added way to get autofill to appear inline with text

--- script.js
+++ script.js
@@ -6,53 +6,122 @@ const suggestions = [
 
 
 var textarea = document.getElementById('myInput')
-var anchorInput = document.getElementById('cursorAnchor')
+var anchor = document.getElementById('cursorAnchor')
+var autoinput = document.getElementById('autoFillInput')
 var input = ''
 
 textarea.addEventListener('keydown', (event) => {
   var key = event.key
-  
-  /* test to check only if a character key was pressed */
-  if (key.length > 1) {
-    if (key === 'Backspace') {
+
+  switch ( key ) {
+    case "Backspace":
       input = input.slice(0, -1);
       console.log("backspace pressed")
-      UpdateAnchorValue(input)
-      console.log("full input: ", input)
-      console.log("anchor text box value: ", anchorInput.value)
-    }
-    return
-  }
-
-  if (key === ' ') {
-    input = ''; 
-    UpdateAnchorValue(input)
-    console.log("full input: ", input)
-    console.log("anchor text box value: ", anchorInput.value)
-  } else {
-    combinedInput = input + key;
-    input = combinedInput;
-    UpdateAnchorValue(input)
-    console.log("full input: ", input)
-    console.log("anchor text box value: ", anchorInput.value)
+      UpdateTextValue(input)
+      break
+    case " ":
+    case "Enter":
+    case "ArrowUp":
+    case "ArrowDown":
+    case "ArrowLeft":
+    case "ArrowRight":
+      input = ""
+      UpdateTextValue(input)
+      break
+    default:
+      if (key.length === 1) {
+        combinedInput = input + key;
+        input = combinedInput;
+        UpdateTextValue(input)
+      }
   }
 })
+  
+  // test to check only if a character key was pressed 
+//   if (key.length > 1) {
+//     if (key === 'Backspace') {
+//       input = input.slice(0, -1);
+//       console.log("backspace pressed")
+//       UpdateTextValue(input)
+//     }
+//     return
+//   }
 
-$( "#cursorAnchor" ).autocomplete({
+//   if (key === ' ') {
+//     //clear input
+//     input = ''; 
+//     UpdateTextValue(input)
+//   } else {
+//     combinedInput = input + key;
+//     input = combinedInput;
+//     UpdateTextValue(input)
+//   }
+// })
+
+$( function() {
+  var availableTags = [
+    "ActionScript",
+    "AppleScript",
+    "Asp",
+    "BASIC",
+    "C",
+    "C++",
+    "Clojure",
+    "COBOL",
+    "ColdFusion",
+    "Erlang",
+    "Fortran",
+    "Groovy",
+    "Haskell",
+    "Java",
+    "JavaScript",
+    "Lisp",
+    "Perl",
+    "PHP",
+    "Python",
+    "Ruby",
+    "Scala",
+    "Scheme"
+];
+  $( "#myInput" ).autocomplete({
   autoFocus: true, 
   position: { of: "#cursorAnchor" }, 
-  source: [ "c++", "java", "php", "coldfusion", "javascript", "asp", "ruby" ],
+  //source: [ "c++", "java", "php", "coldfusion", "javascript", "asp", "ruby" ],
+  source: function( request, response ) {
+    // delegate back to autocomplete, but extract the last term
+    response( $.ui.autocomplete.filter(
+    availableTags, extractLast( request.term ) ) );
+  },
+  focus: function() {
+    // prevent value inserted on focus
+    return false;
+  },
   select: function( event, ui ) {
-    currentPos = event.target.selectionStart();
-    console.log(currentPos)
-    newInput = textarea.value.slice(0, currentPos) + ui.object.value + textarea.value.slice(currentPos)
-    textarea.value = newInput
-  }
-});
+    var terms = split( this.value );
+    // remove the current input
+    terms.pop();
+    // add the selected item
+    terms.push( ui.item.value );
+    // add placeholder to get the comma-and-space at the end
+    terms.push( "" );
+    this.value = terms.join( ", " );
+    return false;
+    }
+  });
+}); 
 
+function split( val ) {
+  return val.split( /,\s*/ );
+}
 
-function UpdateAnchorValue(newInput) {
-  anchorInput.value = newInput;
+function extractLast( term ) {
+  return split( term ).pop();
+}
+
+function UpdateTextValue(newInput) {
+  autoinput.value = newInput;
+  console.log("full input: ", input)
+  console.log("anchor text box value: ", autoinput.value)
 }
 
 /*
